6cefb29c45848c813f7e0d58c38a1306e1a93396
luissancar <luissancar@yahoo.es>
2018-10-08 14:57:24 +0200

    dos

--- app/src/main/java/com/example/luissancar/recyclerviewkotlin2/DatosAdapter.kt
+++ app/src/main/java/com/example/luissancar/recyclerviewkotlin2/DatosAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_datos.view.*
 
 
@@ -31,6 +32,7 @@ class DatosAdapter(val items : ArrayList<String>, val context: Context) : Recycl
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder?.tvDatos?.text = items.get(position)
+        holder?.itemView?.setOnClickListener(View.OnClickListener { Toast.makeText(context,  items.get(position), Toast.LENGTH_SHORT).show() })
 
 
 
